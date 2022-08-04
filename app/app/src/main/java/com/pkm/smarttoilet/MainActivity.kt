d1095a38d6ec42d513c052d312ea928e91845d63
Syn57 <luthfi.n57@gmail.com>
2022-08-04 15:49:07 +0700

    implementing half design of main activity from figma to xml. Finishing tab layout and splash screen logic

--- app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
+++ app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.pkm.smarttoilet
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.pkm.smarttoilet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tips: TextView
+    private lateinit var result: TextView
+    private lateinit var select: TextView
+    private lateinit var def: ColorStateList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        tips = findViewById(R.id.tv_tips_tab)
+        result = findViewById(R.id.tv_result_tab)
+
+
+
+        select = findViewById(R.id.select)
+        def = tips.textColors
+
+        tips.setOnClickListener {
+            tips.setTextColor(resources.getColor(R.color.red))
+            result.setTextColor(resources.getColor(R.color.grey))
+            val size: Int = tips.width
+            select.animate().x(size.toFloat()-3).duration = 200
+        }
+        result.setOnClickListener {
+            select.animate().x(0f).duration = 200
+            tips.setTextColor(resources.getColor(R.color.grey))
+            result.setTextColor(resources.getColor(R.color.red))
+        }
+
+    }
+    fun onClick(v: View){
+
     }
 }
--- app/app/src/main/java/com/pkm/smarttoilet/SplashScreenActivity.kt
+++ app/app/src/main/java/com/pkm/smarttoilet/SplashScreenActivity.kt
@@ -1,11 +1,42 @@
 package com.pkm.smarttoilet
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        hideSystemUI()
+//        Handler().postDelayed({
+//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 2000)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
