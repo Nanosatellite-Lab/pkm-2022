3bff5e8bb14fe22fc688515468a52b73373f3d96
Syn57 <luthfi.n57@gmail.com>
2022-08-05 01:01:59 +0700

    remake splash screen and home header, adding view pager2 an dtab layout for feces section

--- app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
+++ app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
@@ -7,6 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.pkm.smarttoilet.adapter.SectionPagerAdapter
 import com.pkm.smarttoilet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,28 +24,42 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        tips = findViewById(R.id.tv_tips_tab)
-        result = findViewById(R.id.tv_result_tab)
+//        tips = findViewById(R.id.tv_tips_tab)
+//        result = findViewById(R.id.tv_result_tab)
+//
+//
+//
+//        select = findViewById(R.id.select)
+//        def = tips.textColors
 
+        val sectionPagerAdapter = SectionPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager_feces)
+        viewPager.adapter = sectionPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tab_layout_feces)
+        TabLayoutMediator(tabs, viewPager){tab, position->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
 
-
-        select = findViewById(R.id.select)
-        def = tips.textColors
-
-        tips.setOnClickListener {
-            tips.setTextColor(resources.getColor(R.color.red))
-            result.setTextColor(resources.getColor(R.color.grey))
-            val size: Int = tips.width
-            select.animate().x(size.toFloat()-3).duration = 200
-        }
-        result.setOnClickListener {
-            select.animate().x(0f).duration = 200
-            tips.setTextColor(resources.getColor(R.color.grey))
-            result.setTextColor(resources.getColor(R.color.red))
-        }
+//        tips.setOnClickListener {
+//            tips.setTextColor(resources.getColor(R.color.red))
+//            result.setTextColor(resources.getColor(R.color.grey))
+//            val size: Int = tips.width
+//            select.animate().x(size.toFloat()-3).duration = 200
+//        }
+//        result.setOnClickListener {
+//            select.animate().x(0f).duration = 200
+//            tips.setTextColor(resources.getColor(R.color.grey))
+//            result.setTextColor(resources.getColor(R.color.red))
+//        }
 
     }
-    fun onClick(v: View){
 
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_feces_result,
+            R.string.tab_text_feces_tips
+        )
     }
+
 }
