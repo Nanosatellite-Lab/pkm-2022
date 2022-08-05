7c211847f8c6eaf354deb5796d113ed42a706f63
Syn57 <luthfi.n57@gmail.com>
2022-08-05 11:37:13 +0700

    Adding refresh layout

--- app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
+++ app/app/src/main/java/com/pkm/smarttoilet/MainActivity.kt
@@ -1,36 +1,24 @@
 package com.pkm.smarttoilet
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.TextView
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.pkm.smarttoilet.adapter.SectionPagerAdapter
-import com.pkm.smarttoilet.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tips: TextView
-    private lateinit var result: TextView
-    private lateinit var select: TextView
-    private lateinit var def: ColorStateList
+    private lateinit var swipeRefreh: SwipeRefreshLayout
+    private lateinit var hasilDeteski: TextView
+    var num: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
-
-//        tips = findViewById(R.id.tv_tips_tab)
-//        result = findViewById(R.id.tv_result_tab)
-//
-//
-//
-//        select = findViewById(R.id.select)
-//        def = tips.textColors
 
         val sectionPagerAdapter = SectionPagerAdapter(this)
         val viewPager: ViewPager2 = findViewById(R.id.view_pager_feces)
@@ -40,17 +28,14 @@ class MainActivity : AppCompatActivity() {
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
 
-//        tips.setOnClickListener {
-//            tips.setTextColor(resources.getColor(R.color.red))
-//            result.setTextColor(resources.getColor(R.color.grey))
-//            val size: Int = tips.width
-//            select.animate().x(size.toFloat()-3).duration = 200
-//        }
-//        result.setOnClickListener {
-//            select.animate().x(0f).duration = 200
-//            tips.setTextColor(resources.getColor(R.color.grey))
-//            result.setTextColor(resources.getColor(R.color.red))
-//        }
+        hasilDeteski = findViewById(R.id.tv_result)
+        swipeRefreh = findViewById(R.id.refresh_layout)
+        swipeRefreh.setOnRefreshListener {
+            num++
+            hasilDeteski.text = num.toString()
+            swipeRefreh.isRefreshing = false
+        }
+
 
     }
 
